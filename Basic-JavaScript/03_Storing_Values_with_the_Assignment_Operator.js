7d6f96d91822b0b1314e5b6c6362be1a1db80b0a
douglas254 <douglasobara97@gmail.com>
2022-04-23 11:12:45 +0300

    Assigning the Value of One Variable to Another

--- Basic-JavaScript/03_Storing_Values_with_the_Assignment_Operator.js
+++ Basic-JavaScript/03_Storing_Values_with_the_Assignment_Operator.js
@@ -14,3 +14,32 @@ Second, a number value 5 is assigned to the variable called myVar.
 
 
  */
+
+// Assigning the Value of One Variable to Another
+
+/*
+
+    var myVar;
+    myVar = 5;
+    var myNum;
+    myNum = myVar;
+
+The above declares a myVar variable with no value, then assigns it the value 5. Next,
+a variable named myNum is declared with no value. Then, the contents of myVar (which is 5) 
+is assigned to the variable myNum. Now, myNum also has the value of 5.
+
+*/
+
+// Test Quiz
+// Assign the contents of a to variable b.
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+
+b = a; //solution
+
+console.log(b); // output: 7

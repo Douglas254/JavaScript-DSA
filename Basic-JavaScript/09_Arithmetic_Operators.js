9941cf5ba884a967b60e7ada4177cb88d0509b6b
douglas254 <douglasobara97@gmail.com>
2022-04-27 07:17:15 +0300

    Subtract One Number from Another with JavaScript

---
+++ Basic-JavaScript/09_Arithmetic_Operators.js
@@ -0,0 +1,17 @@
+/*
+*******************Subtract One Number from Another with JavaScript*******************
+JavaScript uses the - symbol for subtraction.
+
+E.g
+
+    const myVar = 12 - 6;
+
+myVar would have the value 6.
+ */
+
+// Test Quiz
+// Change the 0 so the difference is 12.
+
+// Solution
+const difference = 45 - 33;
+console.log(difference);

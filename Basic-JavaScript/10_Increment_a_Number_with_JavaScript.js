aebd52ad38e8e59f0200c6a4c39ea5da805537cc
douglas254 <douglasobara97@gmail.com>
2022-04-27 07:48:21 +0300

    Increment a Number with JavaScript

---
+++ Basic-JavaScript/10_Increment_a_Number_with_JavaScript.js
@@ -0,0 +1,23 @@
+/*
+********************Increment a Number with JavaScript********************
+You can easily increment or add one to a variable with the ++ operator.
+Example
+    i++;
+
+is the equivalent of
+
+    i = i + 1;
+
+N/B: The entire line becomes i++;, eliminating the need for the equal sign.
+*/
+// Test Quiz
+// Change the code to use the ++ operator on myVar.
+
+let myVar = 87;
+
+// Only change code below this line
+// myVar = myVar + 1;
+
+// Solution
+myVar++;
+console.log(myVar); //Output: 88

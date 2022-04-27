0d66626360e14ac2334f64a750b6ecd594cc85f9
douglas254 <douglasobara97@gmail.com>
2022-04-27 07:55:22 +0300

    Decrement a Number with JavaScript

---
+++ Basic-JavaScript/11_Decrement_a_Number_with_JavaScript.js
@@ -0,0 +1,23 @@
+/*
+**********************Decrement a Number with JavaScript**********************
+You can easily decrement or decrease a variable by one with the -- operator.
+Example
+
+    i--;
+is the equivalent of
+
+    i = i - 1;
+
+N/B: The entire line becomes i--;, eliminating the need for the equal sign.
+
+*/
+// Test Quiz
+// Change the code to use the -- operator on myVar.
+let myVar = 11;
+
+// Only change code below this line
+// myVar = myVar - 1;
+
+// Solution
+myVar--;
+console.log(myVar); //Output: 10

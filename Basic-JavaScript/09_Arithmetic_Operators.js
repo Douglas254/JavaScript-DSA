c3035b4e211b5d337dd2443193452bce68ab033a
douglas254 <douglasobara97@gmail.com>
2022-04-27 07:25:30 +0300

    Multiply Two Numbers with JavaScript

--- Basic-JavaScript/09_Arithmetic_Operators.js
+++ Basic-JavaScript/09_Arithmetic_Operators.js
@@ -15,3 +15,23 @@ myVar would have the value 6.
 // Solution
 const difference = 45 - 33;
 console.log(difference);
+
+/*
+*******************Multiply Two Numbers with JavaScript*******************
+JavaScript uses the * symbol for multiplication of two numbers.
+
+Example
+
+    const myVar = 13 * 13;
+
+myVar would have the value 169.
+
+*/
+
+// Test Quiz
+// Change the 0 so that product will equal 80.
+// const product = 8 * 0;
+
+// Solution
+const product = 8 * 10;
+console.log(product);
